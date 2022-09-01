4beba403a819f25ded285a12892ce0a6935ea324
Mauro Reis <mauroacsreis@gmail.com>
2022-09-01 16:13:00 -0300

    First Commit

---
+++ pages/_app.js
@@ -0,0 +1,35 @@
+import 'semantic-ui-css/semantic.min.css'
+import '../styles/globals.css'
+import {
+  createClient,
+  configureChains,
+  defaultChains,
+  WagmiConfig
+} from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { SessionProvider } from 'next-auth/react'
+
+const { provider, webSocketProvider } = configureChains(defaultChains, [
+  publicProvider()
+])
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true
+})
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <WagmiConfig client={client}>
+        <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </WagmiConfig>
+    </>
+  )
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,65 @@
+// index.js (next's root route)
+
+// Imports
+import Link from 'next/link'
+import { Container, Header, Button, Card } from 'semantic-ui-react'
+import { useRouter } from 'next/router'
+import campaignFactory from 'utils/ethereum/campaignFactory'
+import HeadComps from 'components/Head/HeadComps'
+import Layout from 'components/Layout/Layout'
+
+export default function Home({ campaigns }) {
+  const router = useRouter()
+
+  const handleCreateCampaign = e => {
+    e.preventDefault()
+    router.push('/campaigns/new')
+  }
+
+  const renderCampaigns = () => {
+    const items = campaigns.map(address => {
+      return {
+        key: address,
+        header: (
+          <Header as="h3" style={{ wordBreak: 'break-all' }}>
+            {address}
+          </Header>
+        ),
+        description: (
+          <Link href={`/campaigns/${address}`}>
+            <a>View Campaign</a>
+          </Link>
+        ),
+        fluid: true
+      }
+    })
+
+    return <Card.Group centered items={items} />
+  }
+
+  return (
+    <Layout>
+      <HeadComps />
+      <Container>
+        <Header as="h3">Open Campaigns</Header>
+        <Button
+          primary
+          content="Create Campaign"
+          icon="add circle"
+          floated="right"
+          onClick={handleCreateCampaign}
+        />
+        {renderCampaigns()}
+      </Container>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps() {
+  const campaigns = await campaignFactory.methods.getDeployedCampaigns().call()
+  return {
+    props: {
+      campaigns
+    }
+  }
+}
